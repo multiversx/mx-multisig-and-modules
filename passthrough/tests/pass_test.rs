17fff874cdfe6a11e8f7183c13b2c4718f4342e0
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-08-12 12:43:51 +0300

    additional test

--- passthrough/tests/pass_test.rs
+++ passthrough/tests/pass_test.rs
@@ -85,3 +85,36 @@ fn execute_without_signatures_test() {
         })
         .assert_ok();
 }
+
+#[test]
+#[should_panic(expected = "only board members and proposers can propose")]
+fn try_execute_action_not_whitelisted() {
+    let mut setup = PassSetup::new(
+        passthrough::contract_obj,
+        multisig_improved::contract_obj,
+        adder::contract_obj,
+    );
+
+    let action_id = setup.propose_add_module(&setup.pass_wrapper.address_ref().clone());
+    setup.sign(action_id, 0);
+    setup.perform(action_id);
+
+    let nice_guy = setup.b_mock.create_user_account(&rust_biguint!(0));
+    let action_id = setup.propose_add_interaction(
+        &setup.adder_wrapper.address_ref().clone(),
+        b"add",
+        vec![nice_guy.clone()],
+    );
+    setup.sign(action_id, 1);
+    setup.perform(action_id);
+
+    let evil_guy = setup.b_mock.create_user_account(&rust_biguint!(0));
+    let args = [[5u8][..].to_vec()].to_vec();
+    setup.propose_transfer_execute_no_sig(
+        &evil_guy,
+        &setup.adder_wrapper.address_ref().clone(),
+        0,
+        b"add",
+        args,
+    );
+}
