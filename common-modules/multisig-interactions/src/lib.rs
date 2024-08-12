7adddba25c641221766ff2aa3cc29f6649d5df31
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-08-12 11:15:35 +0300

    add interaction impl

--- common-modules/multisig-interactions/src/lib.rs
+++ common-modules/multisig-interactions/src/lib.rs
@@ -22,10 +22,24 @@ pub trait MultisigInteractions: only_by_multisig::OnlyByMultisig {
         &self,
         sc_address: ManagedAddress,
         endpoint_name: ManagedBuffer,
-        allowed_token_id: Option<EgldOrEsdtTokenIdentifier>,
+        opt_allowed_token_id: Option<EgldOrEsdtTokenIdentifier>,
         allowed_addresses: MultiValueEncoded<ManagedAddress>,
     ) {
         self.require_multisig_caller();
+        self.require_interaction_not_added(sc_address.clone(), endpoint_name.clone());
+
+        if let Some(token_id) = &opt_allowed_token_id {
+            require!(token_id.is_valid(), "Invalid token ID");
+        }
+
+        let sc_id = self.sc_address_to_id().get_id_or_insert(&sc_address);
+        self.add_allowed_users_for_interaction(sc_id, &endpoint_name, allowed_addresses);
+        self.allowed_token(sc_id, &endpoint_name)
+            .set(opt_allowed_token_id);
+        self.all_interactions().insert(Interaction {
+            sc_address,
+            endpoint_name,
+        });
     }
 
     #[endpoint(disableInteraction)]
@@ -46,6 +60,20 @@ pub trait MultisigInteractions: only_by_multisig::OnlyByMultisig {
         allowed_addresses: MultiValueEncoded<ManagedAddress>,
     ) {
         self.require_multisig_caller();
+    }
+
+    #[endpoint(setAllowedTokenForInteraction)]
+    fn set_allowed_token_for_interaction(
+        &self,
+        sc_address: ManagedAddress,
+        endpoint_name: ManagedBuffer,
+        opt_allowed_token_id: Option<EgldOrEsdtTokenIdentifier>,
+    ) {
+        self.require_multisig_caller();
+
+        let sc_id = self.sc_address_to_id().get_id_non_zero(&sc_address);
+        self.allowed_token(sc_id, &endpoint_name)
+            .set(opt_allowed_token_id);
     }
 
     #[view(canExecute)]
@@ -100,6 +128,38 @@ pub trait MultisigInteractions: only_by_multisig::OnlyByMultisig {
             Some(allowed_token) => OptionalValue::Some(allowed_token),
             None => OptionalValue::None,
         }
+    }
+
+    fn add_allowed_users_for_interaction(
+        &self,
+        sc_id: AddressId,
+        endpoint_name: &ManagedBuffer,
+        allowed_addresses: MultiValueEncoded<ManagedAddress>,
+    ) {
+        let mut interaction_user_mapper = self.allowed_users_for_interaction(sc_id, &endpoint_name);
+        for user in allowed_addresses {
+            interaction_user_mapper.insert(user);
+        }
+    }
+
+    fn require_interaction_not_added(
+        &self,
+        sc_address: ManagedAddress,
+        endpoint_name: ManagedBuffer,
+    ) {
+        let sc_id = self.sc_address_to_id().get_id(&sc_address);
+        if sc_id == NULL_ID {
+            return;
+        }
+
+        let interaction = Interaction {
+            sc_address,
+            endpoint_name,
+        };
+        require!(
+            !self.all_interactions().contains(&interaction),
+            "Interaction already added"
+        );
     }
 
     #[storage_mapper("scAddressToId")]
